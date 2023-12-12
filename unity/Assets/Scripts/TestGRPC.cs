418d4af0aa5d54568c2272f637857cd632cfd282
曾思信 <zengsixin@manlinggame.com>
2023-12-12 18:01:15 +0800

    拼通 mysql

--- unity/Assets/Scripts/TestGRPC.cs
+++ unity/Assets/Scripts/TestGRPC.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class TestGRPC : MonoBehaviour
 {
-   
-    async void Start()
+
+    async void HelloWorld()
     {
         Debug.LogError("---开始---");
         var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions()
@@ -17,10 +17,45 @@ public class TestGRPC : MonoBehaviour
         var client = new Greeter.GreeterClient(channel);
         var res = await client.SayHelloAsync(new HelloRequest()
         {
-            Name = "造就helloworld"
+            Name = "开始例子:helloWorld"
         });
         Debug.LogError(res.Message);
         Debug.LogError("---结束---");
     }
-    
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("hello world"))
+        {
+            HelloWorld();
+        }
+
+        if (GUILayout.Button("hello mySql yes"))
+        {
+            HelloMySql("1234");
+        }
+        if (GUILayout.Button("hello mySql no"))
+        {
+            HelloMySql("z1001000");
+        }
+    }
+
+    async void HelloMySql(string pwd)
+    {
+        Debug.LogError("---开始---");
+        var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions()
+        {
+            HttpHandler = new GRPCBestHttpHandler()
+        });
+
+        var client = new Roleer.RoleerClient(channel);
+        var res = await client.RoleLoginAsync(new ()
+        {
+            Account="z1001",
+            Pwd=pwd
+        });
+        Debug.LogError("账号登录="+(res.State==1?"成功":"失败"));
+        Debug.LogError("---结束---");
+    }
+
 }
